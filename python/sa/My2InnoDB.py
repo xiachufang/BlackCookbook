495ad779fa4bb3eafef91aa18007bfa6ce14b5ad
March Liu <march.liu@gmail.com>
2012-04-09 21:58:54 +0800

    修正数据库名错误

--- python/sa/My2InnoDB.py
+++ python/sa/My2InnoDB.py
@@ -3,7 +3,7 @@
 #将mysql数据库中所有的 myisam 表升级为 innodb
 import sqlalchemy as sa
 #示例中的 mysql 服务器的超级用户只允许操作系统信任登录
-database = 'db'
+db = 'db'
 engine=sa.create_engine("mysql://127.0.0.1/%s"%db)
 for row in engine.execute("SHOW TABLE STATUS from  %s where engine='MyISAM'"%db):
     print "upgrade %s's engine"%row["name"]
